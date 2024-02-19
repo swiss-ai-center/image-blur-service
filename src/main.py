b9d5ccf6e5a08d232f131e04a74fe0972c221c11
Andrea Petrucci <andrea.petrucci@hefr.ch>
2024-02-19 14:29:20 +0100

    Added docs_url field in service description (#6)

--- src/main.py
+++ src/main.py
@@ -61,7 +61,8 @@ class MyService(Service):
                     acronym=ExecutionUnitTagAcronym.IMAGE_PROCESSING
                 ),
             ],
-            has_ai=False
+            has_ai=False,
+            docs_url="https://docs.swiss-ai-center.ch/reference/services/image-blur/",
         )
         self._logger = get_logger(settings)
 
